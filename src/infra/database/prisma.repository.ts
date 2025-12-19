5dc2b9fa71e43b609f66c467d43a9414c9d70612
nattanjunior <fnatanieljunior@gmail.com>
2025-12-19 00:23:56 -0300

    feat: implement PrismaService class for exchange repository

---
+++ src/infra/database/prisma.repository.ts
@@ -0,0 +1,7 @@
+import type { PrismaExchangeRepository } from "src/modules/exchange/domain/prisma-repository";
+
+export class PrismaService implements PrismaExchangeRepository {
+  save(): void {}
+
+  delete(): void {}
+}
